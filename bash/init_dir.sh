367d6b2e00f4ab600261ff4bf705a2be7fc5fb77
ein blatt <einblattx@gmail.com>
2025-09-19 16:21:38 +0800

    init: 添加脚本配置

---
+++ bash/init_dir.sh
@@ -0,0 +1,5 @@
+#初始化默认目录
+mkdir /usr/local/docker /usr/local/docker/app
+cd ../dev-ops
+cp -r mysql nginx redis /usr/local/docker
+cd ../bash
---
+++ bash/install.sh
@@ -0,0 +1,4 @@
+#安装dev-ops中的环境
+./init_dir.sh
+./install_env.sh
+./run_docker.sh
