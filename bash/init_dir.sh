8762c9610274c0bb96e6c2756a8fa9c24aae6a45
ein blatt <einblattx@gmail.com>
2025-09-23 09:47:12 +0800

    fix: 修复初始化目录异常

--- bash/init_dir.sh
+++ bash/init_dir.sh
@@ -1,5 +1,13 @@
 #初始化默认目录
-mkdir /usr/local/docker /usr/local/docker/app
-cd ../dev-ops
+init_dir=/usr/local/docker
+mkdir -p "$init_dir" "$init_dir/app"
+if [ $? -eq 0 ]; then
+    echo "初始化目录：$init_dir, $init_dir/app"
+else
+    echo "目录创建失败"
+    exit 1
+fi
+
+cd ../dev-ops/docker
 cp -r mysql nginx redis /usr/local/docker
-cd ../bash
+cd ../../bash
