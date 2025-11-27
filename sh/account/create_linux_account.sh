2f6b2a4f285abd8178f313c37133abe1c30d4642
ein blatt <einblattx@gmail.com>
2025-11-27 16:03:02 +0800

    refactor(nginx): 优化nginx配置

--- sh/account/create_linux_account.sh
+++ sh/account/create_linux_account.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-USER="front"
+USER="modifyapp"
 HOME_DIR="/usr/local/docker/nginx/app"
-PASSWORD="front"
+PASSWORD="@linux..110"
 
 echo "正在创建用户 $USER ..."
 useradd -m -d "$HOME_DIR" -s /bin/bash "$USER"
 echo "$USER:$PASSWORD" | chpasswd
-chmod 755 "$HOME_DIR"
-chown -R "$USER":"$USER" "$HOME_DIR"
+chown -R $USER:$USER $HOME_DIR
+chmod -R 777 "$HOME_DIR"
+
 
 echo "用户 $USER 创建完成！"
-echo "家目录: $HOME_DIR"
+echo "home目录: $HOME_DIR"
 echo "初始密码: $PASSWORD"
-echo "权限已改为 777"
+echo "权限已改为 700"
